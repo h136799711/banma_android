709bd5462d55fd0a73f0efa9919c8295d8bbfdb4
MrLiuZhifa <wohenhaoma1234@163.com>
2015-10-09 22:07:44 +0800

    提交

---
+++ src/com/itboye/banma/entity/CartList.java
@@ -0,0 +1,19 @@
+package com.itboye.banma.entity;
+
+public class CartList {
+	public String id;//购物车id
+	public String uid;
+	public String create_time;//创建时间
+	public String update_time;
+	public String store_id;//店铺id
+	public String p_id;//商品id
+	public String sku_id;//商品sku_id
+	public String sku_desc ;//商品的文字描述
+	public String icon_url;//商品图片地址
+	public String count;
+	public String name;
+	public String express;//运费
+	public String price;//现价
+	public String ori_price;
+	public String psku_id;//商品规格id
+}
